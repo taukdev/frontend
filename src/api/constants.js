6aaf5388fee68631beb9903390ef1099a0ecc32d
miral-dhameliya-2425 <miraldhameliya2@gmail.com>
2025-07-01 21:07:00 +0530

    set dashbord graph

--- src/api/constants.js
+++ src/api/constants.js
@@ -45,9 +45,9 @@
 // API Configuration
 
 // Set base API URL using environment variable or fallback
-// export const API_URL = process.env.VITE_API_URL || "https://api.taukdash.com";
+export const API_URL = process.env.VITE_API_URL || "https://api.taukdash.com";
 // Base URL for API from environment variable or fallback
-export const API_URL = process.env.REACT_VITE_API || "http://localhost:5000";
+// export const API_URL = process.env.REACT_VITE_API || "http://localhost:5000";
 
 console.log("api key === ", process.env.REACT_VITE_API);
 
